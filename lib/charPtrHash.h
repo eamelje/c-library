cecca328cafe29f023c27452341fa1cf76428076
Eamelje <hdvnld@gmail.com>
2024-03-23 17:26:02 +0100

    update charPtrHash

--- lib/charPtrHash.h
+++ lib/charPtrHash.h
@@ -3,6 +3,9 @@
 
 
 
+#include "../lib/int_isOdd.h"
+
+
 #define ____FUNCTION_DECLARATION_TEMPLATE_charPtrHash(size_) \
 ui ## size_ ## _t \
 charPtrHash ## size_\
@@ -12,6 +15,13 @@ ui32_t ptrSize,\
 ui32_t hashTableSize_Nullable\
 )
 
+
+#if 'a' < 'A'
+#define ____202403231715_CHAR_MIN_VALUE 'a'
+#else
+#define ____202403231715_CHAR_MIN_VALUE 'A'
+#endif
+#define ____202403231715_MAGICVALUE (____202403231715_CHAR_MIN_VALUE - 1)
 
 
 ____FUNCTION_DECLARATION_TEMPLATE_charPtrHash(8);
@@ -29,27 +39,63 @@ ____FUNCTION_DECLARATION_TEMPLATE_charPtrHash(64);
 ui ## size_ ## _t proposed_hash = 0;\
 ui32_t indexMax = ptrSize - 1, index = 0;\
 ui8_t index_step_size;\
-if(indexMax > 250){index_step_size = 10;}\
-if(indexMax > 100){index_step_size = 5;}\
-else if(indexMax > 25){index_step_size = 3;}\
-else {index_step_size = 1;}\
-if( (hashTableSize_Nullable > 0) && (ptrSize & 1) )/* is odd */\
+\
+if(indexMax <= 25)\
+{\
+	index_step_size = 1;\
+}\
+else if(indexMax <= 100)\
+{\
+	index_step_size = 3;\
+}\
+else if(indexMax <= 250)\
+{\
+	index_step_size = 5;\
+}\
+else\
+{\
+	index_step_size = (ptrSize / 25);\
+}\
+\
+\
+\
+if(\
+	(hashTableSize_Nullable > 0)\
+	&&\
+	ui ## size_ ## _isOdd(hashTableSize_Nullable)\
+)\
 {\
 proposed_hash += (_ui ## size_ ## _max_value / 2);\
 }\
+\
+\
+\
+char tempChar;\
+\
+\
 _LABEL_A:\
-proposed_hash += (index % 17)\
-proposed_hash += charPtr[index];\
+tempChar = (charPtr[index] - ____202403231715_MAGICVALUE);\
+proposed_hash += tempChar;\
+proposed_hash *= (tempChar % 7);\
 if(index <= indexMax)\
 {\
 	index += index_step_size;\
 	goto _LABEL_A;\
 }\
+\
+\
+\
 if(hashTableSize_Nullable > 0)\
 {\
 	proposed_hash %= hashTableSize_Nullable;\
 }\
+\
+\
+\
 return proposed_hash;\
+\
+\
+\
 }
 
 
