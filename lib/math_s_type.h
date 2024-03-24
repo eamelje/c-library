aafdef035045df048e16adcfc16dc94eaeadbccc
eamelje <134155892+eamelje@users.noreply.github.com>
2024-03-24 14:06:07 +0100

    Update math_s_type.h

--- lib/math_s_type.h
+++ lib/math_s_type.h
@@ -11,18 +11,40 @@ math_s_status_underflow
 }
 math_s_status_type;
 
-___errargs (status, bool exitearly)
+
+
+
+____math_s_decl_errArgs \
+math_s_status_type *Out_status_Nullable,\
+bool In_exitIfOverflow,\
+bool In_exitIfUnderflow,\
+bool *Out_didExit_Nullable\
+
+
 
 ____math_s_int_decl_part1_template(name_,suffix_,returnType_) \
-__ ## attributes ## __ ## (( ## __ ## always_inline ## __ ## )) inline \
-returnType_ math_s_int_ ## name_ ## _ ## returnType_ ## _ ## suffix_
+__ ## attributes ## __ \
+(( ## __ ## always_inline ## __ ## )) \
+inline \
+returnType_ \
+math_s_int_ ## name_ ## _ ## returnType_ ## _ ## suffix_
 
-#define ____math_s_int_decl_part2_2arg_template(argType_) \
-( argType_ x, argType_ y, math_s_status_type *statusPtr_Nullable )
 
-____math_s_decl_full_2arg_template(name_,suffix_,returnType_,argType) \
-part1() \
-part2_2arg()
+
+#define ____math_s_int_decl_args_2arg_template(argType_) \
+(\
+argType_ x,\
+argType_ y,\
+____math_s_decl_errArgs\
+)
+
+
+
+____math_s_decl_full_2arg_template(name_,suffix_,type_) \
+____math_s_int_decl_part1_template(name_,suffix_,type_) \
+____math_s_int_decl_args_2arg_template(type_)
+
+
 
 
 
