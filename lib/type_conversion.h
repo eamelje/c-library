0cd5d30dba11e8badca4d01807c868a99e2ac43c
eamelje <134155892+eamelje@users.noreply.github.com>
2024-03-26 23:07:49 +0100

    Update type_conversion.h

--- lib/type_conversion.h
+++ lib/type_conversion.h
@@ -1,23 +1,125 @@
 #define convert(oldValue, newType) \
-________ ## conv ## ____getConv(typeof(oldValue),newValue) ##
+________conv_ ## \
+________getConv(typeof(oldValue),newType) ## \
+(oldValue,newType)
 
 
 #include "int_type.h"
-
-#define ________convert_simple(oldValue, newType) ((newType)oldValue)
-#define ________convert_simple_unsafe(
+#include "exit_status_type.h"
 
 
-#define ____getConv(oldType, newType) \
-________convCompl ## _oldType_ ## newType
+#define ________convert_simple(oldValue,newType) \
+((newType)oldValue)
 
-#define ________convCompl_ui8_t_ui8_t simple
-#define ui8_t_ui16_t
-ui8_t_ui32_t
-ui8_t_ui64_t
 
-ui16_t_ui8_t simple_unsafe
-ui16_t_ui16_t
-ui16_t_ui32_t
-ui16_t_ui64_t
+#define ________convert_simple_unsafe(oldValue,newType) /*idk*/
+
+
+#define ________convert_maybeUnderflow(oldValue,newType) \
+(oldValue<0)\
+?\
+{fprintf(stderr,"can't assign negative value to unsigned integer .\n"), exit(exitStatus_failure), 0}\
+:\
+________convert_simple(oldValue,newType)
+
+
+
+#define maybeOverflow
+
+#define ________convert_complex(typeof(oldValue,newType) \ ________convert_complex_ ## \
+typeof(oldValue) ## \
+_ ## \
+newType ## \
+( ## oldValue ## )
+
+
+#define ________getConv(oldType, newType) \
+________convertMode ## _oldType_ ## newType
+
+#define ________convertMode_ui8_t_ui8_t nothing
+#define ________convertMode_ui8_t_ui16_t simple
+#define ________convertMode_ui8_t_ui32_t simple
+#define ________convertMode_ui8_t_ui64_t simple
+
+#define ________convertMode_ui16_t_ui8_t simple_unsafe
+#define ________convertMode_ui16_t_ui16_t nothing
+#define ________convertMode_ui16_t_ui32_t simple
+#define ________convertMode_ui16_t_ui64_t simple
+
+#define ________convertMode_ui32_t_ui8_t simple_unsafe
+#define ________convertMode_ui32_t_ui16_t simple_unsafe
+#define ________convertMode_ui32_t_ui32_t nothing
+#define ________convertMode_ui32_t_ui64_t simple
+
+#define ________convertMode_ui64_t_ui8_t simple_unsafe
+#define ________convertMode_ui64_t_ui16_t simple_unsafe
+#define ________convertMode_ui64_t_ui32_t simple_unsafe
+#define ________convertMode_ui64_t_ui64_t nothing
+
+
+
+#define ________convertMode_si8_t_si8_t nothing
+#define ________convertMode_si8_t_si16_t simple
+#define ________convertMode_si8_t_si32_t simple
+#define ________convertMode_si8_t_si64_t simple
+
+#define ________convertMode_si16_t_si8_t simple_unsafe
+#define ________convertMode_si16_t_si16_t nothing
+#define ________convertMode_si16_t_si32_t simple
+#define ________convertMode_si16_t_si64_t simple
+
+#define ________convertMode_si32_t_si8_t simple_unsafe
+#define ________convertMode_si32_t_si16_t simple_unsafe
+#define ________convertMode_si32_t_si32_t nothing
+#define ________convertMode_si32_t_si64_t simple
+
+#define ________convertMode_si64_t_si8_t simple_unsafe
+#define ________convertMode_si64_t_si16_t simple_unsafe
+#define ________convertMode_si64_t_si32_t simple_unsafe
+#define ________convertMode_si64_t_si64_t nothing
+
+
+
+#define ________convertMode_ui8_t_si8_t simple_unsafe
+#define ________convertMode_ui8_t_si16_t simple
+#define ________convertMode_ui8_t_si32_t simple
+#define ________convertMode_ui8_t_si64_t simple
+
+#define ________convertMode_ui16_t_si8_t simple_unsafe
+#define ________convertMode_ui16_t_si16_t simple_unsafe
+#define ________convertMode_ui16_t_si32_t simple
+#define ________convertMode_ui16_t_si64_t simple
+
+#define ________convertMode_ui32_t_si8_t simple_unsafe
+#define ________convertMode_ui32_t_si16_t simple_unsafe
+#define ________convertMode_ui32_t_si32_t simple_unsafe
+#define ________convertMode_ui32_t_si64_t simple
+
+#define ________convertMode_ui64_t_si8_t simple_unsafe
+#define ________convertMode_ui64_t_si16_t simple_unsafe
+#define ________convertMode_ui64_t_si32_t simple_unsafe
+#define ________convertMode_ui64_t_si64_t simple_unsafe
+
+
+
+
+#define ________convertMode_si8_t_ui8_t maybeUnderflow
+#define ________convertMode_si8_t_ui16_t maybeUnderflow
+#define ________convertMode_si8_t_ui32_t maybeUnderflow
+#define ________convertMode_si8_t_ui64_t maybeUnderflow
+
+#define ________convertMode_si16_t_ui8_t maybeUnderflow
+#define ________convertMode_si16_t_ui16_t maybeUnderflow
+#define ________convertMode_si16_t_ui32_t maybeUnderflow
+#define ________convertMode_si16_t_ui64_t maybeUnderflow
+
+#define ________convertMode_si32_t_ui8_t maybeUnderflow
+#define ________convertMode_si32_t_ui16_t maybeUnderflow
+#define ________convertMode_si32_t_ui32_t maybeUnderflow
+#define ________convertMode_si32_t_ui64_t maybeUnderflow
+
+#define ________convertMode_si64_t_ui8_t maybeUnderflow
+#define ________convertMode_si64_t_ui16_t maybeUnderflow
+#define ________convertMode_si64_t_ui32_t maybeUnderflow
+#define ________convertMode_si64_t_ui64_t maybeUnderflow
 
